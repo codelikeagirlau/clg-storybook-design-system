6108509b35db5737a3356fddde2a72000538d336
SB <sarahbrown.slb@gmail.com>
2023-02-23 10:59:09 +1030

    updated Username component for typechecking

--- src/components/Username.js/Username.js
+++ src/components/Username.js/Username.js
@@ -1,13 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./username.css";
+
+/**
+ * A username component that accepts a single string
+ */
 
 const Username = (props) => {
   return (
     <>
       <div className="username">
-        <p>{props.username_text || "clg user"}</p>
+        <p>{props.username_text}</p>
       </div>
     </>
   );
 };
 export default Username;
+
+Username.propTypes = {
+  username: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }),
+};
